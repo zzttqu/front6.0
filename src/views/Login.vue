beb918cd6953f275f02bb69fca27307b040275ac
zzttqu <80448114+zzttqu@users.noreply.github.com>
2022-05-11 11:11:31 +0800

    登录页面增加了表单

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,19 +8,38 @@
         <i class="iconfont" style="font-size: 2rem">&#xe64f;</i>
       </button>
     </div>
+    <v-form ref="form" label-position="center" label-width="auto" class="form">
+      <v-text-field
+          v-model="user.userInfo"
+          :rules="[v=>!!v||'用户名还没填哦']"
+          label="用户名或邮箱"
+          required
+      >
+      </v-text-field>
+      <v-text-field
+          v-model="user.password"
+          type="password"
+          :rules="[v=>!!v||'需要填密码']"
+          label="密码"
+      >
+      </v-text-field>
+    </v-form>
   </div>
 </template>
 
 <script>
+import {reactive} from "vue";
 
 export default {
   name: "Login",
   components: {},
   setup() {
-
-
+    let user = reactive({
+      userInfo: "",
+      password: ""
+    });
     return {
-
+      user
     };
   }
 }
@@ -30,11 +49,12 @@ export default {
 
 <style lang="scss" scoped>
 
-.navBar{
+.navBar {
   display: flex;
   justify-content: space-around;
   padding-top: 0.5rem;
 }
+
 .noClick {
   pointer-events: none;
 }
