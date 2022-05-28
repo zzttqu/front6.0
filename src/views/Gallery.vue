800ed8e9cafb1e1ea7f0f11f0b28e04a03578316
zzttqu <80448114+zzttqu@users.noreply.github.com>
2022-05-28 10:08:20 +0800

    完成了转圈加载图标，加载更多的按钮bug完全修复。

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -5,18 +5,19 @@
       <div class="column">
         <v-card tag="div" v-for="(img,index) in imgUrlA" @click="ab" class="animate__animated animate__fadeIn">
           <v-progress-circular
-              v-if="img.url===undefined"
+              v-if="img.loading===true"
               indeterminate=""
               color="grey"
               class="loading"
           >
           </v-progress-circular>
-          <img :src="img.url"/>
+          <img :src="img.url" @load="imgLoad();img.loading=false" alt="展示图片缩略图"/>
           <div class="title">{{ img.title }}</div>
         </v-card>
         <div ref="positionA">
         </div>
-        <v-card class="load-more" v-if="heightDiff<=0" :height="-heightDiff-10" @click="ab">
+        <v-card class="load-more animate__animated animate__fadeIn" v-if="heightDiff<=0" :height="-heightDiff-10"
+                @click="ab">
           <div>
             <v-icon style="font-size: 3rem">mdi-dots-horizontal</v-icon>
           </div>
@@ -24,12 +25,20 @@
       </div>
       <div class="column">
         <v-card v-for="(img,index) in imgUrlB" @click="ab">
-          <img :src="img.url">
+          <v-progress-circular
+              v-if="img.loading===true"
+              indeterminate=""
+              color="grey"
+              class="loading"
+          >
+          </v-progress-circular>
+          <img :src="img.url" @load="imgLoad();img.loading=false" alt="展示图片缩略图"/>
           <div class="title">{{ img.title }}</div>
         </v-card>
         <div ref="positionB">
         </div>
-        <v-card class="load-more" v-if="heightDiff>0" :height="heightDiff-10" @click="ab">
+        <v-card class="load-more animate__animated animate__fadeIn" v-if="heightDiff>0" :height="heightDiff-10"
+                @click="ab">
           <div>
             <v-icon style="font-size: 3rem">mdi-dots-horizontal</v-icon>
           </div>
@@ -41,7 +50,7 @@
 
 <script>
 import Header from "../components/Header";
-import {nextTick, onMounted, ref} from "vue";
+import {onMounted, ref} from "vue";
 import axios from "axios";
 
 
@@ -52,42 +61,38 @@ export default {
     let positionA = ref();
     let positionB = ref();
     let heightDiff = ref();
+    let loading = ref(0);
     onMounted(() => {
       //从后端获取图片的地址
-      axios.get("https://i.loli.net/2021/10/15/UaueVTkfrHwCjiM.jpg").then(() => {
+      axios.get("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fyaya-img.diqiu00.com%2Fuploads%2Fimage%2F20211127%2F1637975574594597.jpg&refer=http%3A%2F%2Fyaya-img.diqiu00.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1656295896&t=7383fa9b852c6ed4fd6ec60fa179dc07").then(() => {
         let a = [
           {url: "https://s2.loli.net/2022/01/21/iPB2rjVqoewm9yn.jpg", title: "123"},
           {url: "https://i.loli.net/2021/10/15/UaueVTkfrHwCjiM.jpg", title: "title"},
-          {url: "https://i.loli.net/2021/10/15/UaueVTkfrHwCjiM.jpg", title: "title"},
-          {url: "https://s2.loli.net/2022/01/21/iPB2rjVqoewm9yn.jpg", title: "123"},
-          {url: "https://s2.loli.net/2022/01/21/iPB2rjVqoewm9yn.jpg", title: "123"},
         ];
-        //创建图片的缓存并缓存
-        const images = a.map(imgSrc => {
-          return new Promise((resolve, reject) => {
-            const img = new Image();
-            img.src = imgSrc.url;
-            img.onload = resolve;
-            img.onerror = reject;
-          });
-        });
-        //缓存结束后再分列展示
-        Promise.all(images).then(res => {
-          spliceImg(a);
-          nextTick(() => {
-            heightDiff.value = positionA.value.getBoundingClientRect().y - positionB.value.getBoundingClientRect().y;
-          });
-        });
-
+        a.reduce((total, value) => {
+          value.loading = true;
+        }, 0);
+        spliceImg(a);
+        //创建图片的缓存并缓存，这个是得在别的页面提前加载这个页面的才有用
+        // const images = a.map(imgSrc => {
+        //   return new Promise((resolve, reject) => {
+        //     const img = new Image();
+        //     img.src = imgSrc.url;
+        //     img.onload = resolve;
+        //     img.onerror = reject;
+        //   });
+        // });
+        // 缓存结束后再分列展示
+        // Promise.all(images).then(res => {
+        //
+        // });
       });
     });
 
-    let imgUrlA = ref([{}, {}, {}, {}]);
-    let imgUrlB = ref([{}, {}, {}, {}]);
+    let imgUrlA = ref([]);
+    let imgUrlB = ref([]);
 
     function spliceImg(imgList) {
-      imgUrlB.value = [];
-      imgUrlA.value = [];
       imgList.filter((value, index) => {
         if (index % 2 === 0) {
           imgUrlA.value.push(value);
@@ -109,6 +114,15 @@ export default {
       positionA,
       positionB,
       heightDiff,
+      loading,
+      imgLoad() {
+        loading.value += 1;
+        if (loading.value === imgUrlA.value.length + imgUrlB.value.length) {
+          // console.log("图片全部加载完毕");
+          heightDiff.value = positionA.value.getBoundingClientRect().y - positionB.value.getBoundingClientRect().y;
+          // console.log(heightDiff.value);
+        }
+      }
     };
   }
 };
